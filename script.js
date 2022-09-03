25814e9e4b92b28173c5544fd09d72e5873ed784
MarufHacker <112495572+Marufytjutf@users.noreply.github.com>
2022-09-03 12:58:44 +0300

    Bu fayl qo'shildi

---
+++ script.js
@@ -0,0 +1,6 @@
+function carta() {
+    onclick = prompt("Turgan joyingiz")
+    prompt("Raqamingiz")
+    prompt("Qaysi paytda yetkazib berilsin")
+    alert("Sovg'angizni 5-soat ichida olasiz")
+}
